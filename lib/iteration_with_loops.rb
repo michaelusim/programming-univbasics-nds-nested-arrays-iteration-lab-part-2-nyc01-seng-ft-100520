e2d3c321a8b08f773ba3efe64036f4b73932b29d
michael onyeije <monyeije16@saintpeters.edu>
2020-09-29 17:06:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,10 +1,9 @@
 def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
+  arr = []
   array_of_daily_temperatures.each_with_index { |i,index|
   arr[index]= i.min
 }
 
-print arr
+return arr
 
 end
